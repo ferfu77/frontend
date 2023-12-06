126c4ba8986d97fbc4b18bb2b6225db7958636ab
ferfu77 <ferchuft2@hotmail.com>
2023-12-05 23:26:38 -0300

    Unidades

--- src/Edificio.js
+++ src/Edificio.js
@@ -4,6 +4,36 @@ import UnidadesPorEdificio from './UnidadesEdificio'; // Importa el componente U
 function ListaEdificios() {
   const [codigoEdificio, setCodigoEdificio] = useState('');
   const [edificios, setEdificios] = useState([]);
+  const [nuevoEdificio, setNuevoEdificio] = useState({
+    nombre: '',
+    direccion: ''
+    // Agrega más propiedades según la estructura de tu edificio
+  })
+
+  const guardarEdificio = () => {
+    fetch('http://localhost:8080/api/edificios/guardar', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(nuevoEdificio),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Aquí puedes manejar la respuesta del servidor si es necesario
+        console.log('Edificio guardado:', data);
+        // Podrías actualizar la lista de edificios luego de guardar
+        // O volver a cargar los edificios con la llamada a la API
+      })
+      .catch((error) => {
+        console.error('Error al guardar el edificio:', error);
+      });
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setNuevoEdificio({ ...nuevoEdificio, [name]: value });
+  };
 
   useEffect(() => {
     fetch('http://localhost:8080/api/edificios/listar')
@@ -40,6 +70,25 @@ function ListaEdificios() {
           ))}
         </tbody>
       </table>
+      <div>
+        <h3>Guardar Nuevo Edificio</h3>
+        <input
+          type="text"
+          name="nombre"
+          value={nuevoEdificio.nombre}
+          onChange={handleInputChange}
+          placeholder="Nombre del edificio"
+        />
+        <input
+          type="text"
+          name="direccion"
+          value={nuevoEdificio.direccion}
+          onChange={handleInputChange}
+          placeholder="Dirección del edificio"
+        />
+        {/* Agrega más inputs según las propiedades del edificio */}
+        <button onClick={guardarEdificio}>Guardar Edificio</button>
+      </div>
     </div>
   );
 }
